7a2b382f95fe7dca7d1cdc45822d0d93c2ede242
J.Teeuwissen <jelleteeuwissen@hotmail.nl>
2022-02-09 16:57:24 +0100

    Added main module haddock documentation (#3)

--- src/JsonLogic.hs
+++ src/JsonLogic.hs
@@ -1,3 +1,10 @@
+-- |
+-- Module      : JsonLogic
+-- Description : JsonLogic library for Haskell
+-- Copyright   : (c) Marien Matser, Gerard van Schie, Jelle Teeuwissen 2022
+-- License     : MIT
+-- Maintainer  : jelleteeuwissen@hotmail.nl
+-- Stability   : experimental
 module JsonLogic (flip3) where
 
 -- | Flips the elements of a 3-tuple.
