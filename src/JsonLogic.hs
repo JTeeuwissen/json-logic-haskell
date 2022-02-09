40f01d988830f1e15c1255126e5d68630fb6f033
Jelle Teeuwissen <jelleteeuwissen@hotmail.nl>
2022-02-09 12:10:25 +0100

    Added ormolu linting

--- src/JsonLogic.hs
+++ src/JsonLogic.hs
@@ -1,4 +1,9 @@
 module JsonLogic (flip3) where
 
-flip3 :: (c, b, a) -> (a, b, c)
+-- | Flips the elements of a 3-tuple.
+flip3 ::
+  -- | The 'tuple' argument
+  (c, b, a) ->
+  -- | The return value
+  (a, b, c)
 flip3 (x, y, z) = (z, y, x)
