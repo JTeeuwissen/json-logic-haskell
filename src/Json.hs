1718c93a4a3c5c6bbfc8bb101980d3009fcb22b6
Jelle Teeuwissen <jelleteeuwissen@hotmail.nl>
2022-02-19 08:56:54 +0100

    FFT: rewrote using either for error

--- src/Json.hs
+++ src/Json.hs
@@ -2,29 +2,46 @@ module Json where
 
 import qualified Data.Map as M
 
--- Same definition
-type Rule = Json
+-- A rule can only be a JSON object.
+type Rule = Object
 
+-- Data can be any kind of JSON value.
 type Data = Json
 
--- Same definition
+-- Json is a collection of possivle JSON values.
 data Json
   = JsonNull
   | JsonBool Bool
   | JsonNumber Double
   | JsonString String
   | JsonArray [Json]
-  | JsonObject (M.Map String Json)
+  | JsonObject Object
   deriving (Eq, Show)
 
-type Function = [Json] -> Maybe Json
+-- A Json object, dictionary with string keys and Json values.
+type Object = (M.Map String Json)
+
+type Function = Json -> FunctionResult
 
 -- Contains the functions are variables our environment has currently
 data JsonLogicEnv = JLEnv
-  { functions :: M.Map String ([Json] -> Maybe Json), -- Either Json String), -- All the operations (plus custom ones)
+  { functions :: M.Map String Function, -- All the operations (plus custom ones)
     variables :: Json -- Variables defined in rules
   }
 
 -- Cannot derive itself, so empty instance
 instance Show JsonLogicEnv where
   show _ = ""
+
+data EvalError = EvalError
+  { functionName :: String,
+    paramaters :: Json,
+    errorMessage :: String
+  }
+  deriving (Show, Eq)
+
+type FunctionError = String
+
+type EvalResult = Either EvalError Json
+
+type FunctionResult = Either FunctionError Json
