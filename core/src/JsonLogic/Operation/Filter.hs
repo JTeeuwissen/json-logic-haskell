53a1924b721162ca6c053ebb38499062b3e07c5e
Jelle Teeuwissen <jelleteeuwissen@hotmail.nl>
2022-02-25 13:36:28 +0100

    CRF: using filterM

--- core/src/JsonLogic/Operation/Filter.hs
+++ core/src/JsonLogic/Operation/Filter.hs
@@ -1,24 +1,13 @@
 module JsonLogic.Operation.Filter where
 
+import Control.Monad (filterM)
 import Control.Monad.Except (MonadError (throwError))
-import Data.Maybe (catMaybes)
 import JsonLogic.Json (Function, Json (JsonArray))
 import JsonLogic.Operation.Primitive (evaluateArray, evaluateBool)
 
 evaluateFilter :: Function
 evaluateFilter evaluator (JsonArray [xs, f]) vars = do
   array <- evaluateArray evaluator xs vars
-  filtered <- filterEither (evaluateBool evaluator f) array
+  filtered <- filterM (evaluateBool evaluator f) array
   return $ JsonArray filtered
 evaluateFilter _ _ _ = throwError "Wrong number of arguments for filter"
-
-filterEither :: (x -> Either e Bool) -> [x] -> Either e [x]
-filterEither f xs = do
-  maybes <-
-    traverse
-      ( \x -> do
-          x' <- f x
-          return $ if x' then Just x else Nothing
-      )
-      xs
-  return $ catMaybes maybes
