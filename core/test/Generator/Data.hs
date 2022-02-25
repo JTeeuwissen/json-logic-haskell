2ce16f9ebd7ef5b69437658238ae9cb6a84ebb33
Gerard van Schie <g.j.vanschie@students.uu.nl>
2022-02-25 11:11:03 +0100

    FFT: Random json data generation (#76)
    * FFT: Random json data generation
    
    * Fix: use error instead of undefined

--- core/test/Generator/Data.hs
+++ core/test/Generator/Data.hs
@@ -1,16 +1,51 @@
+{-# LANGUAGE OverloadedLists #-}
+
 module Generator.Data where
 
+import Data.Map as M (fromList, insert)
 import Generator.Generic
-import Hedgehog
-import Hedgehog.Gen
-import Hedgehog.Range as Range
-import JsonLogic.Json
+  ( genGenericJsonBool,
+    genGenericJsonNumber,
+    genGenericJsonString,
+  )
+import Generator.Utils (genUnbalancedSizeList)
+import Hedgehog (Gen, Size (Size))
+import Hedgehog.Gen (choice)
+import JsonLogic.Json (Json (JsonArray, JsonNull, JsonObject))
+import Text.Read (readMaybe)
 
-genRandomJson :: Gen Json
-genRandomJson = undefined
+-- | Inserts Json into a specific path and returns it
+-- If it is arrived at the end of the path then it gives the value back
+insertAtPath ::
+  -- | The path at which to store the json
+  [String] ->
+  -- | Json data that needs to get inserted
+  Json ->
+  -- | Json object that needs to get updated
+  Json ->
+  -- | Updated Json object with inserted Json
+  Json
+-- End of path reached
+insertAtPath [] value _ = value
+-- Insert value to the map
+insertAtPath (p : ps) value (JsonObject o) = case readMaybe p of
+  Nothing -> JsonObject $ insert p (insertAtPath ps value JsonNull) o
+  Just i -> JsonArray $ replicate (i :: Int) JsonNull ++ [insertAtPath ps value JsonNull]
+-- Insert value into an array
+insertAtPath (p : ps) value (JsonArray js) = case readMaybe p of
+  Nothing -> JsonObject [(p, insertAtPath ps value JsonNull)]
+  Just i -> JsonArray $ replicate ((i :: Int) - length js) JsonNull ++ [insertAtPath ps value JsonNull]
+-- It is inserting along a new path, denoted with JsonNull
+insertAtPath (p : ps) value JsonNull = case readMaybe p of
+  Nothing -> JsonObject [(p, insertAtPath ps value JsonNull)]
+  Just i -> JsonArray $ replicate (i :: Int) JsonNull ++ [insertAtPath ps value JsonNull]
+-- Data is always an array or an object in the top layer, everything else is wrong
+insertAtPath _ _ _ = error "Error invalid Json, your json data is not an array or object"
 
+-- | Generate random Json given a size
 genSizedRandomJson :: Size -> Gen Json
-genSizedRandomJson (Size size)
+genSizedRandomJson s@(Size size)
+  -- If size less or equal to 0 a final item is closed
   | size <= 0 =
       choice
         [ return JsonNull,
@@ -18,4 +53,28 @@ genSizedRandomJson (Size size)
           fst <$> genGenericJsonNumber,
           fst <$> genGenericJsonString
         ]
-  | otherwise = undefined
+  -- If size is greater than 0 we expand with an array or object
+  | otherwise =
+      choice
+        [ genSizedRandomJsonArray s,
+          genSizedRandomJsonObject s
+        ]
+
+-- | Generate a Random sized Json array
+genSizedRandomJsonArray :: Size -> Gen Json
+genSizedRandomJsonArray size = do
+  sizes <- genUnbalancedSizeList size
+  JsonArray <$> mapM genSizedRandomJson sizes
+
+-- | Generate sized Jsonobject entry (pair<key,value>)
+genSizedRandomJsonEntry :: Size -> Gen (String, Json)
+genSizedRandomJsonEntry size = do
+  str <- snd <$> genGenericJsonString
+  json <- genSizedRandomJson size
+  return (str, json)
+
+-- | Generate random Json object with a given size
+genSizedRandomJsonObject :: Size -> Gen Json
+genSizedRandomJsonObject size = do
+  sizes <- genUnbalancedSizeList size
+  JsonObject . M.fromList <$> mapM genSizedRandomJsonEntry sizes
--- core/test/Generator/Generic.hs
+++ core/test/Generator/Generic.hs
@@ -1,34 +1,36 @@
 module Generator.Generic where
 
-import Data.Map as M
 import Hedgehog
 import Hedgehog.Gen
 import Hedgehog.Range as Range
 import JsonLogic.Json
 
+-- | Generator for a JsonNull object
 genGenericJsonNull :: Gen (Json, Json)
 genGenericJsonNull = return (JsonNull, JsonNull)
 
+-- | Generator for a random JsonBool object
 genGenericJsonBool :: Gen (Json, Bool)
 genGenericJsonBool = do
   b <- bool
   return (JsonBool b, b)
 
+-- | Generator for a random JsonNumber object
 genGenericJsonNumber :: Gen (Json, Double)
 genGenericJsonNumber = do
   d <- double $ Range.constantFrom 1 10 100
   return (JsonNumber d, d)
 
+-- | Generator for empty strings
 genGenericEmptyJsonString :: Gen (Json, String)
 genGenericEmptyJsonString = return (JsonString "", "")
 
+-- | Generator for a random JsonString
 genGenericJsonString :: Gen (Json, String)
 genGenericJsonString = do
-  s <- string (Range.constantFrom 1 3 20) alphaNum
+  s <- string (Range.constantFrom 1 3 10) alphaNum
   return (JsonString s, s)
 
+-- | Generator for empty JsonArrays
 genGenericEmptyJsonArray :: Gen (Json, [a])
 genGenericEmptyJsonArray = return (JsonArray [], [])
-
-genGenericJsonObject :: Gen (Json, M.Map String Json)
-genGenericJsonObject = undefined
