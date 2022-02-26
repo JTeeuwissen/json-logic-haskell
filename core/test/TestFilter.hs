8d31859b4e0d99d6f447e05e085c2a79c960f3b3
Jelle Teeuwissen <jelleteeuwissen@hotmail.nl>
2022-02-26 11:23:42 +0100

    FFT: finished tests

--- core/test/TestFilter.hs
+++ core/test/TestFilter.hs
@@ -22,15 +22,95 @@ filterUnitTests =
         U.assertEqual
           "Return True elements"
           (Right $ JsonArray [JsonBool True, JsonBool True])
-          (eval [] (JsonObject [("filter", JsonArray [JsonArray [JsonBool True, JsonBool False, JsonBool True], JsonObject [("var", JsonString "")]])]) JsonNull),
+          ( eval
+              []
+              ( JsonObject
+                  [ ( "filter",
+                      JsonArray
+                        [ JsonArray [JsonBool True, JsonBool False, JsonBool True],
+                          JsonObject [("var", JsonString "")]
+                        ]
+                    )
+                  ]
+              )
+              JsonNull
+          ),
       testCase "Smaller than filter values" $
         U.assertEqual
           "Only the first item is < 2"
           (Right $ JsonArray [JsonNumber 1])
-          (eval [] (JsonObject [("filter", JsonArray [JsonArray [JsonNumber 1, JsonNumber 3], JsonObject [("<", JsonArray [JsonObject [("var", JsonString "")], JsonNumber 2])]])]) JsonNull),
+          ( eval
+              []
+              ( JsonObject
+                  [ ( "filter",
+                      JsonArray
+                        [ JsonArray [JsonNumber 1, JsonNumber 3],
+                          JsonObject [("<", JsonArray [JsonObject [("var", JsonString "")], JsonNumber 2])]
+                        ]
+                    )
+                  ]
+              )
+              JsonNull
+          ),
       testCase "Evaluate array values" $
         U.assertEqual
           "The array value gets evaluated to True and returned."
           (Right $ JsonArray [JsonBool True])
-          (eval [] (JsonObject [("filter", JsonArray [JsonArray [JsonBool False, JsonObject [("<", JsonArray [JsonNumber 1, JsonNumber 2])]], JsonObject [("var", JsonString "")]])]) JsonNull)
+          ( eval
+              []
+              ( JsonObject
+                  [ ( "filter",
+                      JsonArray
+                        [ JsonArray
+                            [ JsonBool False,
+                              JsonObject [("<", JsonArray [JsonNumber 1, JsonNumber 2])]
+                            ],
+                          JsonObject [("var", JsonString "")]
+                        ]
+                    )
+                  ]
+              )
+              JsonNull
+          ),
+      testCase "Empty array" $
+        U.assertEqual
+          "No errors occur when handed an empty array"
+          (Right $ JsonArray [])
+          ( eval
+              []
+              ( JsonObject
+                  [ ( "filter",
+                      JsonArray
+                        [ JsonArray [],
+                          JsonObject [("var", JsonString "")]
+                        ]
+                    )
+                  ]
+              )
+              JsonNull
+          ),
+      testCase "Two consecutive filters" $
+        U.assertEqual
+          "Two consecutive filter more."
+          (Right $ JsonArray [JsonNumber 2])
+          ( eval
+              []
+              ( JsonObject
+                  [ ( "filter",
+                      JsonArray
+                        [ JsonObject
+                            [ ( "filter",
+                                JsonArray
+                                  [ JsonArray [JsonNumber 1, JsonNumber 2, JsonNumber 3],
+                                    JsonObject [("<=", JsonArray [JsonObject [("var", JsonString "")], JsonNumber 2])]
+                                  ]
+                              )
+                            ],
+                          JsonObject [(">=", JsonArray [JsonObject [("var", JsonString "")], JsonNumber 2])]
+                        ]
+                    )
+                  ]
+              )
+              JsonNull
+          )
     ]
