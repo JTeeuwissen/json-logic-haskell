affcd82a523f7b195f28eabb7de25e87afd6b77b
Jelle Teeuwissen <jelleteeuwissen@hotmail.nl>
2022-03-04 15:21:16 +0100

    FIX: truthy/falsy

--- core/test/Operation/TestNegation.hs
+++ core/test/Operation/TestNegation.hs
@@ -2,16 +2,12 @@
 
 module Operation.TestNegation where
 
-import qualified Data.List as L
 import Generator.Data
-import Generator.Generic
 import Hedgehog (forAll, property, (===))
 import qualified Hedgehog.Gen as Gen
-import qualified Hedgehog.Range as Range
 import JsonLogic
 import JsonLogic.Json
 import Test.Tasty
-import Test.Tasty.HUnit as U
 import Test.Tasty.Hedgehog as H
 
 negationGeneratorTests :: TestTree
@@ -21,9 +17,9 @@ negationGeneratorTests =
     [ H.testProperty "negation works" $
         property $ do
           paramJson <- forAll $ Gen.sized genSizedRandomJson
-          Right (JsonBool $ isTruthy paramJson) === eval [] (JsonObject [("!", paramJson)]) JsonNull,
+          Right (JsonBool $ isFalsy paramJson) === eval [] (JsonObject [("!", paramJson)]) JsonNull,
       H.testProperty "double negation works" $
         property $ do
           paramJson <- forAll $ Gen.sized genSizedRandomJson
-          Right (JsonBool $ not $ isTruthy paramJson) === eval [] (JsonObject [("!!", paramJson)]) JsonNull
+          Right (JsonBool $ isTruthy paramJson) === eval [] (JsonObject [("!!", paramJson)]) JsonNull
     ]
