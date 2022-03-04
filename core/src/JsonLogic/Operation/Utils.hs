7d9bf40ed0d9b85423a7e3afefd674a1beca37f8
Jelle Teeuwissen <jelleteeuwissen@hotmail.nl>
2022-03-04 19:02:58 +0100

    FIX: base version lower

--- core/src/JsonLogic/Operation/Utils.hs
+++ core/src/JsonLogic/Operation/Utils.hs
@@ -1,14 +1,13 @@
 module JsonLogic.Operation.Utils where
 
-import qualified Data.List as L (singleton)
 import qualified Data.Map as M (lookup)
 import JsonLogic.Json (Data, Json (..), Rule)
 import Text.Read (readMaybe)
 
 -- | Index a json object using a string seperated by periods.
--- >>> indexJson "x.y" (JsonObject $ M.singleton "x" $ JsonObject $ M.singleton "y" JsonNull)
+-- >>> indexJson "x.y" (JsonObject $ singleton "x" $ JsonObject $ singleton "y" JsonNull)
 -- Just JsonNull
--- >>> indexJson "x.y" (JsonObject $ M.singleton "x" JsonNull)
+-- >>> indexJson "x.y" (JsonObject $ singleton "x" JsonNull)
 -- Nothing
 -- >>> indexJson "" (JsonNumber 1)
 -- Just (JsonNumber 1.0)
@@ -26,7 +25,7 @@ indexWithJson _ = const Nothing
 indexWithString :: [String] -> Data -> Maybe Json
 indexWithString [] vars = Just vars
 indexWithString [x] (JsonString s) =
-  readMaybe x >>= (!?) s >>= Just . JsonString . L.singleton
+  readMaybe x >>= (!?) s >>= Just . JsonString . singleton
 indexWithString (x : xs) (JsonArray js) =
   readMaybe x >>= (!?) js >>= indexWithString xs
 indexWithString (x : xs) (JsonObject o) = M.lookup x o >>= indexWithString xs
@@ -50,3 +49,11 @@ _ !? n | n < 0 = Nothing
 [] !? _ = Nothing
 (x : _) !? 0 = Just x
 (_ : xs) !? n = xs !? (n - 1)
+
+-- | Put a single item in a list
+-- Included in base since: base-4.15.0.0
+-- But currently on older version.
+-- >>> singleton "single value"
+-- ["single value"]
+singleton :: a -> [a]
+singleton x = [x]
