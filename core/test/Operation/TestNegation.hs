8b7c5c34ae8ca3dc59a42bef1752a47733ad3a42
Jelle Teeuwissen <jelleteeuwissen@hotmail.nl>
2022-03-04 16:09:50 +0100

    UFT: unit tests

--- core/test/Operation/TestNegation.hs
+++ core/test/Operation/TestNegation.hs
@@ -8,18 +8,46 @@ import qualified Hedgehog.Gen as Gen
 import JsonLogic
 import JsonLogic.Json
 import Test.Tasty
+import Test.Tasty.HUnit
+import qualified Test.Tasty.HUnit as U
 import Test.Tasty.Hedgehog as H
+
+negationUnitTests :: TestTree
+negationUnitTests =
+  testGroup
+    "negation unit tests"
+    [ testCase "Negated true is false" $
+        U.assertEqual
+          "logic: {\"!\": [true]} data: null"
+          (Right (JsonBool False))
+          (eval [] (JsonObject [("!", JsonArray [JsonBool True])]) JsonNull),
+      testCase "logic: {\"!\": true} data: null" $
+        U.assertEqual
+          "Negated true is false"
+          (Right (JsonBool False))
+          (eval [] (JsonObject [("!", JsonBool True)]) JsonNull),
+      testCase "logic: {\"!!\": [ [] ] } data: null" $
+        U.assertEqual
+          "Empty array is false"
+          (Right (JsonBool False))
+          (eval [] (JsonObject [("!!", JsonArray [JsonArray []])]) JsonNull),
+      testCase "logic: {\"!!\": [\"0\"] } data: null" $
+        U.assertEqual
+          "Non empty array is true"
+          (Right (JsonBool True))
+          (eval [] (JsonObject [("!!", JsonArray [JsonString "0"])]) JsonNull)
+    ]
 
 negationGeneratorTests :: TestTree
 negationGeneratorTests =
   testGroup
-    "negation unit tests"
+    "negation generator tests"
     [ H.testProperty "negation works" $
         property $ do
           paramJson <- forAll $ Gen.sized genSizedRandomJson
-          Right (JsonBool $ isFalsy paramJson) === eval [] (JsonObject [("!", paramJson)]) JsonNull,
+          Right (JsonBool $ isFalsy paramJson) === eval [] (JsonObject [("!", JsonObject [("preserve", paramJson)])]) JsonNull,
       H.testProperty "double negation works" $
         property $ do
           paramJson <- forAll $ Gen.sized genSizedRandomJson
-          Right (JsonBool $ isTruthy paramJson) === eval [] (JsonObject [("!!", paramJson)]) JsonNull
+          Right (JsonBool $ isTruthy paramJson) === eval [] (JsonObject [("!!", JsonObject [("preserve", paramJson)])]) JsonNull
     ]
