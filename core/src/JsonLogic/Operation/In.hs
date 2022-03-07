3198f3cf17711f7ae01592c85a59fd7992b5e239
Marien <matser71@gmail.com>
2022-03-07 16:14:05 +0100

    Add in for other numbers of arguments

--- core/src/JsonLogic/Operation/In.hs
+++ core/src/JsonLogic/Operation/In.hs
@@ -1,13 +1,12 @@
 module JsonLogic.Operation.In where
 
-import Control.Monad.Except (MonadError (throwError))
 import qualified Data.List as L
 import JsonLogic.Json (Function, Json (JsonArray, JsonBool))
 import JsonLogic.Operation.Primitive (evaluateString)
 
 evaluateIn :: Function
-evaluateIn evaluator (JsonArray [sub, string]) vars = do
+evaluateIn evaluator (JsonArray (sub : string : _)) vars = do
   sub' <- evaluateString evaluator sub vars
   string' <- evaluateString evaluator string vars
   return $ JsonBool $ L.isInfixOf sub' string'
-evaluateIn _ _ _ = throwError "In received wrong agruments"
+evaluateIn _ _ _ = return $ JsonBool False
