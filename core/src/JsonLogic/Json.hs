940883a56d369ddf1f01432c63a33642e2b92b18
GerardvSchie <g.j.vanschie@students.uu.nl>
2022-03-07 10:41:12 +0100

    FIX: Resolve requested changes

--- core/src/JsonLogic/Json.hs
+++ core/src/JsonLogic/Json.hs
@@ -27,7 +27,7 @@ instance Show Json where
   show (JsonNumber d) = show d
   show (JsonString s) = show s
   show (JsonArray js) = show js
-  show (JsonObject o) = "{" ++ intercalate "," (map (\(k, v) -> k ++ ":" ++ show v) $ M.toList o) ++ "}"
+  show (JsonObject o) = "{" ++ intercalate "," (map (\(k, v) -> "\"" ++ k ++ "\":" ++ show v) $ M.toList o) ++ "}"
 
 stringify :: Json -> String
 stringify JsonNull = ""
