52212368005a62884fb615b574cb9092e8165dc5
Jelle Teeuwissen <jelleteeuwissen@hotmail.nl>
2022-04-03 18:05:17 +0200

    FFT: finished example

--- example/app/Main.hs
+++ example/app/Main.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE OverloadedLists #-}
+
 module Main where
 
 import JsonLogic.Json
@@ -6,28 +8,36 @@ import JsonLogic.Pure.Operation
 
 -- | The main function
 -- Perform simple power function
--- >>> :{main
--- 4
--- 5
--- :}
--- TODO
 main :: IO ()
 main = do
   putStrLn "First number:"
   first <- getLine
   putStrLn "Second number:"
   second <- getLine
-  print $ evaluatorWithPow (read $ "{\"**\":[" ++ first ++ "," ++ second ++ "]}") JsonNull
+  print $ evaluate (read first) (read second)
+
+-- | Evaluate two numbers with pow operation using json logic.
+-- The two numbers are placed into an data object and given to the evaluator with the following logic:
+-- {"**":[{"var":"base"}, {"var":"exp"}]}
+-- >>> evaluate (read "3") (read "4")
+-- 81
+evaluate :: Json -> Json -> Result Json
+evaluate base expo = evaluatorWithPow (read "{\"**\":[{\"var\":\"base\"}, {\"var\":\"exp\"}]}") (JsonObject [("base", base), ("exp", expo)])
 
 -- | An evaluator that can evaluate operations with power (**).
 evaluatorWithPow :: Rule -> Data -> Result Json
 evaluatorWithPow = eval [powEvaluator]
 
 -- | The power operation.
+-- Takes the power function and adds a name to it to create an operation.
 powEvaluator :: Operation
 powEvaluator = ("**", powFunction)
 
 -- | The power function.
+-- Takes an subevaluator, function arguments (in this case just a list) and data to pass through.
+-- 1. tries to evaluate the arguments to double values
+--  (as they might be json logic evaluating to doubles, instead of direct numbers).
+-- 2. if successful, returns the result of the power operation
 powFunction :: Function Json
 powFunction evaluator (JsonArray [base', expo']) vars = do
   base <- evaluateDouble evaluator base' vars
