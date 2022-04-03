ce343c80dbd8e04fa9dd5eb452cd303f10a00173
Jelle Teeuwissen <jelleteeuwissen@hotmail.nl>
2022-04-03 16:15:51 +0200

    UFT: finished example function

--- example/app/Main.hs
+++ example/app/Main.hs
@@ -2,17 +2,35 @@ module Main where
 
 import JsonLogic.Json
 import JsonLogic.Pure.Evaluator
+import JsonLogic.Pure.Operation
 
+-- | The main function
+-- Perform simple power function
+-- >>> :{main
+-- 4
+-- 5
+-- :}
+-- TODO
 main :: IO ()
 main = do
   putStrLn "First number:"
   first <- getLine
   putStrLn "Second number:"
   second <- getLine
-  print $ evaluatorWithPow (read first) (read second)
+  print $ evaluatorWithPow (read $ "{\"**\":[" ++ first ++ "," ++ second ++ "]}") JsonNull
 
-evaluatorWithPow :: Rule -> Data -> Result
+-- | An evaluator that can evaluate operations with power (**).
+evaluatorWithPow :: Rule -> Data -> Result Json
 evaluatorWithPow = eval [powEvaluator]
 
+-- | The power operation.
 powEvaluator :: Operation
-powEvaluator = ("**", undefined)
+powEvaluator = ("**", powFunction)
+
+-- | The power function.
+powFunction :: Function Json
+powFunction evaluator (JsonArray [base', expo']) vars = do
+  base <- evaluateDouble evaluator base' vars
+  expo <- evaluateDouble evaluator expo' vars
+  return $ JsonNumber $ base ** expo
+powFunction _ _ _ = Left "Wrong number of arguments for **"
