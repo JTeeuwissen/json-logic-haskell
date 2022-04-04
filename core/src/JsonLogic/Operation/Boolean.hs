88ec5ff4304dafa74c1bd8779273138fd2ab8096
Jelle Teeuwissen <jelleteeuwissen@hotmail.nl>
2022-04-04 10:45:42 +0200

    UFT: started implementation

--- core/src/JsonLogic/Operation/Boolean.hs
+++ core/src/JsonLogic/Operation/Boolean.hs
@@ -18,9 +18,9 @@ if' = ("if", evaluateIf)
 
 -- Implementation for bool -> bool -> bool operators
 (==), (===), (!=), (!==), (!), (!!), and, or :: Monad m => Operation m
-(==) = ("==", undefined) -- TODO eq
+(==) = ("==", looseEquals)
 (===) = ("==", evaluateLogic (P.==))
-(!=) = ("!=", undefined) -- TODO neq
+(!=) = ("!=", looseNotEquals)
 (!==) = ("!=", evaluateLogic (P./=))
 (!) = ("!", evaluateFalsey)
 (!!) = ("!!", evaluateTruthy)
@@ -36,10 +36,10 @@ evaluateIf _ _ _ = throwError "Wrong number of arguments for if"
 -- Helper functions
 
 evaluateLogic :: Monad m => (Bool -> Bool -> Bool) -> Function m Json
-evaluateLogic operator evaluator (JsonArray [x, y]) vars = do
-  x' <- evaluateBool evaluator x vars
-  y' <- evaluateBool evaluator y vars
-  return $ JsonBool $ x' `operator` y'
+evaluateLogic operator evaluator (JsonArray [x', y']) vars = do
+  x <- evaluateBool evaluator x' vars
+  y <- evaluateBool evaluator y' vars
+  return $ JsonBool $ x `operator` y
 evaluateLogic _ _ _ _ = throwError "Wrong number of arguments for logic operator"
 
 evaluateTruthy :: Monad m => Function m Json
@@ -47,3 +47,48 @@ evaluateTruthy evaluator json vars = JsonBool <$> evaluateBool evaluator (evalua
 
 evaluateFalsey :: Monad m => Function m Json
 evaluateFalsey evaluator json vars = JsonBool . not <$> evaluateBool evaluator (evaluateUnaryArgument json) vars
+
+-- | Evaluate loose equals
+looseEquals :: Monad m => Function m Json
+looseEquals evaluator (JsonArray [x', y']) vars = do
+  x <- evaluator x' vars
+  y <- evaluator y' vars
+  return $
+    JsonBool $ looseEq x y
+looseEquals _ _ _ = throwError "Wrong number of arguments for loose not equals operator"
+
+looseNotEquals :: Monad m => Function m Json
+looseNotEquals evaluator (JsonArray [x', y']) vars = do
+  x <- evaluator x' vars
+  y <- evaluator y' vars
+  return $
+    JsonBool $ not $ looseEq x y
+looseNotEquals _ _ _ = throwError "Wrong number of arguments for loose not equals operator"
+
+-- | See: https://github.com/gregsdennis/json-everything/blob/master/JsonLogic/JsonElementExtensions.cs#L117
+-- See: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Equality
+looseEq :: Json -> Json -> Bool
+looseEq (JsonArray a) (JsonArray b) = a P.== b
+looseEq (JsonObject a) (JsonObject b) = a P.== b
+looseEq (JsonNumber a) (JsonNumber b) = a P.== b
+looseEq (JsonString a) (JsonString b) = a P.== b
+looseEq (JsonBool a) (JsonBool b) = a P.== b
+looseEq JsonNull JsonNull = True
+looseEq JsonNull _ = False
+looseEq _ JsonNull = False
+looseEq (JsonObject _) _ = False
+looseEq _ (JsonObject _) = False
+looseEq a@(JsonNumber _) (JsonArray b) = looseEq a (JsonString $ show b)
+looseEq (JsonArray a) b@(JsonNumber _) = looseEq (JsonString $ show a) b
+looseEq a@(JsonNumber _) b = a P.== numberify b
+looseEq a b@(JsonNumber _) = numberify a P.== b
+looseEq (JsonArray a) (JsonString b) = show a P.== b
+looseEq (JsonString a) (JsonArray b) = a P.== show b
+looseEq _ _ = False
+
+-- See https://github.com/gregsdennis/json-everything/blob/master/JsonLogic/JsonElementExtensions.cs#L84
+numberify :: Json -> Json
+numberify (JsonString s) = JsonNumber (read s)
+numberify n@(JsonNumber _) = n
+numberify (JsonBool b) = JsonNumber $ toEnum $ fromEnum b
+numberify _ = JsonNull
