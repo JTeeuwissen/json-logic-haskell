6189a5b8fa2f6b98a0858ab4ead68d5b8fddc8d4
Jelle Teeuwissen <jelleteeuwissen@hotmail.nl>
2022-04-04 17:12:37 +0200

    FFT: no more JL

--- core/src/JsonLogic/Evaluator.hs
+++ core/src/JsonLogic/Evaluator.hs
@@ -1,36 +1,21 @@
 module JsonLogic.Evaluator (apply) where
 
 import Control.Monad.Except
-import Control.Monad.Reader
 import Data.Map as M
-import JsonLogic.JL
 import JsonLogic.Json
 import JsonLogic.Type
 
--- evaluate JsonLogic without bothering about monads
-apply :: Monad m => Operations m -> Rule -> Json -> Result m Json
-apply ops rule d = runReader (evalRule rule) $ JLEnv ops d
-
 -- | Evaluate a rule
 -- Evaluate an object or array, return other items.
-evalRule :: Monad m => Rule -> JL (Result m Json) m
-evalRule o@(JsonObject rule) = do
-  result <- sequenceA <$> traverseWithKey evalFunc rule
-  -- An empty rule returns itself
-  return $ M.foldr const o <$> result -- FIX: this only returns a single rule result. Maybe evaluate one.
-evalRule (JsonArray rules) = do
-  result <- sequenceA <$> mapM evalRule rules
-  return $ JsonArray <$> result
-evalRule x = return $ return x
-
-evalFunc :: Monad m => String -> Json -> JL (Result m Json) m
-evalFunc fName param = do
-  ops <- getOperations
-  vars <- getVariables
-  function <- getFunction fName
-  return $ case function of
-    Nothing -> throwError $ UnrecognizedOperation fName
-    Just f -> f (subEval ops) param vars
-
-subEval :: Monad m => Operations m -> Rule -> Data -> Result m Json
-subEval ops rule d = runReader (evalRule rule) $ JLEnv ops d
+apply :: Monad m => Operations m -> Rule -> Data -> Result m Json
+apply ops (JsonObject rules) dat = case M.toList rules of
+  [(fName, fRule)] -> do
+    case M.lookup fName ops of
+      Nothing -> throwError $ UnrecognizedOperation fName
+      Just f -> f (apply ops) fRule dat
+  -- A rule should always contain only one field
+  _ -> throwError $ InvalidRule (M.keys rules)
+apply ops (JsonArray rules) dat = do
+  result <- mapM (\rule -> apply ops rule dat) rules
+  return $ JsonArray result
+apply _ x _ = return x
