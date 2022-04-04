6cd7bc1b423be76753a0029d6cf121f8b64040b0
Jelle Teeuwissen <jelleteeuwissen@hotmail.nl>
2022-04-04 10:57:53 +0200

    FIX: comma

--- core/test/Operation/Boolean/TestEquality.hs
+++ core/test/Operation/Boolean/TestEquality.hs
@@ -10,14 +10,13 @@ equalityUnitTests :: TestTree
 equalityUnitTests =
   testGroup
     "Read json unit tests"
-    [
-       testCase "Numbers" $ assertEqual (JsonNumber 1) (JsonNumber 1) True,
-       testCase "Strings" $ assertEqual (JsonString "hello") (JsonString "hello") True,
-       testCase "Strings and Numbers" $ assertEqual (JsonString "1") (JsonNumber 1) True,
-       testCase "Numbers and Bools" $ assertEqual (JsonNumber 0) (JsonBool False) True,
-       testCase "Numbers and Null" $ assertEqual (JsonNumber 0) (JsonNull) False,
-       testCase "Numbers and not not Null" $ assertEqual (JsonNumber 0) (JsonObject [("!!", JsonNull)]) True,
-       testCase "Objects" $ assertEqual (JsonObject [("key", JsonString "value")]) (JsonObject [("key", JsonString "value")]) True,
+    [ testCase "Numbers" $ assertEqual (JsonNumber 1) (JsonNumber 1) True,
+      testCase "Strings" $ assertEqual (JsonString "hello") (JsonString "hello") True,
+      testCase "Strings and Numbers" $ assertEqual (JsonString "1") (JsonNumber 1) True,
+      testCase "Numbers and Bools" $ assertEqual (JsonNumber 0) (JsonBool False) True,
+      testCase "Numbers and Null" $ assertEqual (JsonNumber 0) (JsonNull) False,
+      testCase "Numbers and not not Null" $ assertEqual (JsonNumber 0) (JsonObject [("!!", JsonNull)]) True,
+      testCase "Objects" $ assertEqual (JsonObject [("key", JsonString "value")]) (JsonObject [("key", JsonString "value")]) True
     ]
 
 assertEqual :: Json -> Json -> Bool -> Assertion
