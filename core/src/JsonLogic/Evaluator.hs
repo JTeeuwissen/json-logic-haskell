056c918acc662d91b76a325d298f3efc6867620e
Jelle Teeuwissen <jelleteeuwissen@hotmail.nl>
2022-04-04 17:58:22 +0200

    FIX: return object if not a single key

--- core/src/JsonLogic/Evaluator.hs
+++ core/src/JsonLogic/Evaluator.hs
@@ -8,13 +8,13 @@ import JsonLogic.Type
 -- | Evaluate a rule
 -- Evaluate an object or array, return other items.
 apply :: Monad m => Operations m -> Rule -> Data -> Result m Json
-apply ops (JsonObject rules) dat = case M.toList rules of
+apply ops o@(JsonObject rules) dat = case M.toList rules of
   [(fName, fRule)] -> do
     case M.lookup fName ops of
       Nothing -> throwError $ UnrecognizedOperation fName
       Just f -> f (apply ops) fRule dat
   -- A rule should always contain only one field
-  _ -> throwError $ InvalidRule (M.keys rules)
+  _ -> return o -- throwError $ InvalidRule (M.keys rules)
 apply ops (JsonArray rules) dat = do
   result <- mapM (\rule -> apply ops rule dat) rules
   return $ JsonArray result
