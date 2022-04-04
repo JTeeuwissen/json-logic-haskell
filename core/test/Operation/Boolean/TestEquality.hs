a6376706ac38d221263f159eb6d0ec6af06ced5e
Jelle Teeuwissen <jelleteeuwissen@hotmail.nl>
2022-04-04 11:03:23 +0200

    FIX: typing

--- core/test/Operation/Boolean/TestEquality.hs
+++ core/test/Operation/Boolean/TestEquality.hs
@@ -1,9 +1,12 @@
+{-# LANGUAGE OverloadedLists #-}
+
 module Operation.Boolean.TestEquality where
 
+import JsonLogic.Json
+import JsonLogic.Pure.Evaluator
 import Test.Tasty (TestTree, testGroup)
 import Test.Tasty.HUnit (testCase)
 import qualified Test.Tasty.HUnit as U
-import Utils
 
 -- See https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Equality
 equalityUnitTests :: TestTree
@@ -14,14 +17,14 @@ equalityUnitTests =
       testCase "Strings" $ assertEqual (JsonString "hello") (JsonString "hello") True,
       testCase "Strings and Numbers" $ assertEqual (JsonString "1") (JsonNumber 1) True,
       testCase "Numbers and Bools" $ assertEqual (JsonNumber 0) (JsonBool False) True,
-      testCase "Numbers and Null" $ assertEqual (JsonNumber 0) (JsonNull) False,
+      testCase "Numbers and Null" $ assertEqual (JsonNumber 0) JsonNull False,
       testCase "Numbers and not not Null" $ assertEqual (JsonNumber 0) (JsonObject [("!!", JsonNull)]) True,
       testCase "Objects" $ assertEqual (JsonObject [("key", JsonString "value")]) (JsonObject [("key", JsonString "value")]) True
     ]
 
-assertEqual :: Json -> Json -> Bool -> Assertion
+assertEqual :: Json -> Json -> Bool -> U.Assertion
 assertEqual l r b =
   U.assertEqual
     "Result is correct"
-    (JsonBool b)
+    (Right $ JsonBool b)
     (eval [] (JsonObject [("==", JsonArray [l, r])]) JsonNull)
