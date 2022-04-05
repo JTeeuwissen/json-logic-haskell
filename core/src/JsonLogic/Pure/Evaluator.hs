ea0f9fff702d1ffcf7a5042479b9995979145d2a
J Teeuwissen <jelleteeuwissen@hotmail.nl>
2022-04-05 12:16:44 +0200

    FFT: applyEmpty (#156)
    * FFT: applyEmpty
    
    * FIX: back to using list

--- core/src/JsonLogic/Pure/Evaluator.hs
+++ core/src/JsonLogic/Pure/Evaluator.hs
@@ -5,7 +5,7 @@
 -- License     : MIT
 -- Maintainer  : jelleteeuwissen@hotmail.nl
 -- Stability   : experimental
-module JsonLogic.Pure.Evaluator (apply) where
+module JsonLogic.Pure.Evaluator (apply, applyEmpty) where
 
 import qualified Data.Map as M
 import qualified JsonLogic.Evaluator as E
@@ -17,4 +17,9 @@ import JsonLogic.Pure.Type
 -- >>> apply [] (read "{\"trace\":\"Hello, World!\"}":: Json) JsonNull
 -- Right "Hello, World!"
 apply :: [Operation] -> Rule -> Data -> Result Json
-apply ops rule d = toResult $ E.apply (M.map fromFunction (M.union (M.fromList ops) defaultOperations)) rule d
+apply ops = applyEmpty (ops ++ M.toList defaultOperations)
+
+-- >>> applyEmpty [] (read "{\"log\":\"Hello, World!\"}":: Json) JsonNull
+-- Left (UnrecognizedOperation {operationName = "log"})
+applyEmpty :: [Operation] -> Rule -> Data -> Result Json
+applyEmpty ops rule dat = toResult $ E.apply (M.map fromFunction $ M.fromList ops) rule dat
