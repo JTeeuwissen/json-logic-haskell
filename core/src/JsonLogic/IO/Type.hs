328b7d2ca77b57328deafb43ebb0539f75b98358
Jelle Teeuwissen <jelleteeuwissen@hotmail.nl>
2022-04-05 21:35:35 +0200

    DOC: more rename to Json

--- core/src/JsonLogic/IO/Type.hs
+++ core/src/JsonLogic/IO/Type.hs
@@ -15,13 +15,13 @@ import qualified JsonLogic.Type as T
 -- | A result is an exception or a value wrapped in IO.
 type Result r = IO (Either T.Exception r)
 
--- | A Subevaluator takes a rule and data and returns a result of JSON.
+-- | A Subevaluator takes a rule and data and returns a result of Json.
 type SubEvaluator = Rule -> Data -> Result Json
 
 -- | A function takes a subevaluator, rule and data and returns a result.
 type Function r = SubEvaluator -> Rule -> Data -> Result r
 
--- | An operation is a JSON function with a name.
+-- | An operation is a Json function with a name.
 type Operation = (String, Function Json)
 
 -- | Operations is a Map from the operation name to the operation function.
--- core/src/JsonLogic/Pure/Type.hs
+++ core/src/JsonLogic/Pure/Type.hs
@@ -14,13 +14,13 @@ import qualified JsonLogic.Type as T
 -- | A result is an exception or a value.
 type Result r = Either T.Exception r
 
--- | A subevaluator takes a rule and data and returns a result of JSON.
+-- | A subevaluator takes a rule and data and returns a result of Json.
 type SubEvaluator = Rule -> Data -> Result Json
 
 -- | A function takes a subevaluator, rule and data and returns a result.
 type Function r = SubEvaluator -> Rule -> Data -> Result r
 
--- | An operation is a JSON function with a name.
+-- | An operation is a Json function with a name.
 type Operation = (String, Function Json)
 
 -- | Operations is a Map from the operation name to the operation function.
