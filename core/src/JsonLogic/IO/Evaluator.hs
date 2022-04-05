f13798f4ad9addfed4766e9cb83becfb221f63fa
Jelle Teeuwissen <jelleteeuwissen@hotmail.nl>
2022-04-05 10:51:03 +0200

    FIX: back to using list

--- core/src/JsonLogic/IO/Evaluator.hs
+++ core/src/JsonLogic/IO/Evaluator.hs
@@ -1,4 +1,4 @@
-module JsonLogic.IO.Evaluator (apply) where
+module JsonLogic.IO.Evaluator (apply, applyEmpty) where
 
 import qualified Data.Map as M
 import qualified JsonLogic.Evaluator as E
@@ -7,12 +7,12 @@ import JsonLogic.IO.Operation (defaultOperations)
 import JsonLogic.IO.Type
 import JsonLogic.Json
 
--- >>> apply M.empty (read "{\"log\":\"Hello, World!\"}":: Json) JsonNull
+-- >>> apply [] (read "{\"log\":\"Hello, World!\"}":: Json) JsonNull
 -- Right "Hello, World!"
-apply :: Operations -> Rule -> Data -> Result Json
-apply ops = applyEmpty (M.union ops defaultOperations)
+apply :: [Operation] -> Rule -> Data -> Result Json
+apply ops = applyEmpty (ops ++ M.toList defaultOperations)
 
--- >>> applyEmpty M.empty (read "{\"log\":\"Hello, World!\"}":: Json) JsonNull
+-- >>> applyEmpty [] (read "{\"log\":\"Hello, World!\"}":: Json) JsonNull
 -- Left (UnrecognizedOperation {operationName = "log"})
-applyEmpty :: Operations -> Rule -> Data -> Result Json
-applyEmpty ops rule dat = toResult $ E.apply (M.map fromFunction ops) rule dat
+applyEmpty :: [Operation] -> Rule -> Data -> Result Json
+applyEmpty ops rule dat = toResult $ E.apply (M.map fromFunction $ M.fromList ops) rule dat
--- core/src/JsonLogic/Pure/Evaluator.hs
+++ core/src/JsonLogic/Pure/Evaluator.hs
@@ -5,14 +5,14 @@ import qualified JsonLogic.Evaluator as E
 import JsonLogic.Json
 import JsonLogic.Pure.Mapping
 import JsonLogic.Pure.Operation
-import JsonLogic.Pure.Type (Operations, Result)
+import JsonLogic.Pure.Type
 
--- >>> apply M.empty(read "{\"trace\":\"Hello, World!\"}":: Json) JsonNull
+-- >>> apply [] (read "{\"trace\":\"Hello, World!\"}":: Json) JsonNull
 -- Right "Hello, World!"
-apply :: Operations -> Rule -> Data -> Result Json
-apply ops = applyEmpty (M.union ops defaultOperations)
+apply :: [Operation] -> Rule -> Data -> Result Json
+apply ops = applyEmpty (ops ++ M.toList defaultOperations)
 
--- >>> applyEmpty M.empty (read "{\"log\":\"Hello, World!\"}":: Json) JsonNull
+-- >>> applyEmpty [] (read "{\"log\":\"Hello, World!\"}":: Json) JsonNull
 -- Left (UnrecognizedOperation {operationName = "log"})
-applyEmpty :: Operations -> Rule -> Data -> Result Json
-applyEmpty ops rule dat = toResult $ E.apply (M.map fromFunction ops) rule dat
+applyEmpty :: [Operation] -> Rule -> Data -> Result Json
+applyEmpty ops rule dat = toResult $ E.apply (M.map fromFunction $ M.fromList ops) rule dat
